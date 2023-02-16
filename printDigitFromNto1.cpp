02a1d954a41c7e8733837d3b666f4931a8687b46
naveensingh575 <57229365+naveensingh575@users.noreply.github.com>
2023-02-16 22:28:48 +0530

    Create printDigitFromNto1.cpp

---
+++ printDigitFromNto1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+  //Take no. of digit to print from user
+  cout << "Enter the no. of digit to be print ";
+  int num;
+  cin >> num;
+  for(int i =num; i>0;i--)
+  {
+    cout << i << '\n';
+  }
+  return 0;
+}
+    
