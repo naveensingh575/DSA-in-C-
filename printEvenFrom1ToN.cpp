1d08ea77b51186b31d0b48fae6dc8dc780d56bc2
naveensingh575 <57229365+naveensingh575@users.noreply.github.com>
2023-02-18 20:28:51 +0530

    Create printEvenFrom1ToN.cpp

---
+++ printEvenFrom1ToN.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int number;
+  cout << "Enter no. of digit to be print ";
+  cin >> number;
+  for(int i =2;i<=number;i =i+2)
+  {
+    cout << i;
+  }
+  return 0;
+}
+    
+    
