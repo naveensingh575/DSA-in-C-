d7e76be0bfba6e72ce6c8f77ea5842d56b935e04
naveensingh575 <57229365+naveensingh575@users.noreply.github.com>
2023-02-21 14:02:31 +0530

    Update hollowRectangularPattern.cpp

--- hollowRectangularPattern.cpp
+++ hollowRectangularPattern.cpp
@@ -2,31 +2,36 @@
 using namespace std;
 
 int main() {
-  for(int row =0; row < 3; row++)
+  int rowNum, colNum;
+  cout << "Enter number of row: ";
+  cin >> rowNum;
+  cout << "Enter number of col: ";
+  cin >> colNum;
+  
+  for(int row =0;row<rowNum;row++)
   {
-    // fiest and last row
-     if(row ==0 || row ==2)
-     {
-       for(int col =0; col <5; col++)
-       {
-         cout << "*";
-       }
-     }
-     else
-     {
-       //print first *
-       cout << "*";
-
-       for(int col =1; col < 4; col++)
-       {
-         //print space
-         cout << " ";
-      
-       }
-       //print last *
-       cout <<"*";
-     }
+      if((row ==0) || (row == rowNum-1))
+      {
+        for(int col =0;col<colNum;col++)
+        {
+          cout << "* ";
+        }
+      } 
+      else
+      {
+        for(int col =0;col<colNum;col++)
+	      {
+      	  if((col == 0) || (col == (colNum-1)))
+      	  {
+      		cout << "* ";
+      	  }
+      	  else
+      	  {
+      		cout << "  ";
+      	  }
+	      }
+      }
     cout << endl;
   }
-  
+  return 0;
 }
